f9f63a94a5fc871b1386adc9bccc3726522f8914
Slevin <yann.debray@gadz.org>
2021-03-07 12:12:47 +0100

    Access public S3 bucket

---
+++ 2_prepare_data/upload-aws-s3.py
@@ -0,0 +1,16 @@
+import os
+import boto3
+
+s3_client = boto3.client('s3')
+s3_bucket = "aiworkflow"
+
+training_dataset = "2021-03-06-1"
+
+training_files = os.listdir("samples/"+training_dataset)
+
+for file in training_files:
+    file_name = "samples/"+training_dataset+"/"+file
+    object_name = training_dataset+"_"+file
+    print(object_name)
+    # s3_client.upload_file(file_name, bucket, object_name)
+    s3_client.upload_file(file_name, s3_bucket, object_name)
